7f6177766053da2d199c30487fafab3d4a859500
Rita00 <rita.lapao00@gmail.com>
2021-11-25 23:38:07 +0000

    update bd and sql script

--- BD/createTables.sql
+++ BD/createTables.sql
@@ -1,3 +1,8 @@
+DROP TABLE IF EXISTS client;
+DROP TABLE IF EXISTS manager;
+DROP TABLE IF EXISTS currency;
+DROP TABLE IF EXISTS payment_credit;
+
 CREATE TABLE client (
                         id	 BIGINT,
                         username	 VARCHAR(512) UNIQUE NOT NULL,
@@ -19,4 +24,17 @@ CREATE TABLE currency (
                           PRIMARY KEY(id)
 );
 
+CREATE TABLE payment_credit (
+                                id		 BIGINT,
+                                payment_date	 DATE NOT NULL,
+                                price		 FLOAT(8) NOT NULL,
+                                credit_id		 BIGINT UNIQUE NOT NULL,
+                                credit_credit_date DATE NOT NULL,
+                                currency_id	 BIGINT NOT NULL,
+                                client_id		 BIGINT NOT NULL,
+                                PRIMARY KEY(id)
+);
+
 ALTER TABLE client ADD CONSTRAINT client_fk1 FOREIGN KEY (manager_id) REFERENCES manager(id);
+ALTER TABLE payment_credit ADD CONSTRAINT payment_credit_fk1 FOREIGN KEY (currency_id) REFERENCES currency(id);
+ALTER TABLE payment_credit ADD CONSTRAINT payment_credit_fk2 FOREIGN KEY (client_id) REFERENCES client(id);
