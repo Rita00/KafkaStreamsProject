9bdff5bc45763ef8b7fb9e561f7710f897499176
lbpaisDev <lbpaisdev@gmail.com>
2021-12-02 15:17:59 +0000

    Clean code and streams implemented for kafka

--- Clients/src/main/java/Clients.java
+++ Clients/src/main/java/Clients.java
@@ -8,24 +8,24 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import Entities.Client;
 import org.json.JSONObject;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
 
 public class Clients {
 
     public static void main(String[] args) throws Exception {
+        //Create instance for random
         Random rand = new Random();
+        //Create instance for gson
         Gson gson = new Gson();
 
-        //Setup properties to produce to both topics
-        //Assign Credits and Payments as name topics
+        //Set topics names
         String cTopic = "Credits";
         String pTopic = "Payments";
-        // create instance for properties to access producer configs
+        String dbTopic = "DBInfoTopics";
+
+        //Create instance for properties to access producer configs
         Properties propsProducer = new Properties();
+        //Setup properties to produce to both topics
         //Assign localhost id
         propsProducer.put("bootstrap.servers", "localhost:9092");
         //Set acknowledgements for producer requests.
@@ -43,86 +43,81 @@ public class Clients {
         //Create producer with previous properties
         Producer<Long, Float> producer = new KafkaProducer<>(propsProducer);
 
-        //Setup properties to consume from DBInfoTopics
-        //Assign topicName to string variable
-        String dbTopic = "DBInfoTopics";
+
         // create instance for properties to access producer configs
-        Properties props = new Properties();
+        Properties propsConsumer = new Properties();
         //Assign localhost id
-        props.put("bootstrap.servers", "localhost:9092");
+        propsConsumer.put("bootstrap.servers", "localhost:9092");
         //Set acknowledgements for producer requests.
-        props.put("acks", "all");
+        propsConsumer.put("acks", "all");
         //If the request fails, the producer can automatically retry,
-        props.put("retries", 0);
+        propsConsumer.put("retries", 0);
         //Specify buffer size in config
-        props.put("batch.size", 16384);
+        propsConsumer.put("batch.size", 16384);
         //Reduce the no of requests less than 0
-        props.put("linger.ms", 1);
+        propsConsumer.put("linger.ms", 1);
         //The buffer.memory controls the total amount of memory available to the producer for buffering.
-        props.put("buffer.memory", 33554432);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaExampleConsumer");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.LongDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        Consumer<Long, String> dbConsumer = new KafkaConsumer<>(props);
+        propsConsumer.put("buffer.memory", 33554432);
+        propsConsumer.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaDBConsumer");
+        propsConsumer.put("key.deserializer", "org.apache.kafka.common.serialization.LongDeserializer");
+        propsConsumer.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        //Create the consumer with previous settings
+        Consumer<Long, String> dbConsumer = new KafkaConsumer<>(propsConsumer);
+
+        //Subscribe to the DBInfoTopics
         dbConsumer.subscribe(Collections.singletonList(dbTopic));
 
         //List of client ids
         ArrayList<Long> clientIds = new ArrayList<>();
 
         float cred, pay;
-        int sleepTime, index;
+        int sleepTime = 5000;
         long clientId;
 
         while (true) {
-            //From each record we only want the client Id
+            //Fetch all data from the DBInfoTopics
             System.out.println("Getting clients from " + dbTopic);
             ConsumerRecords<Long, String> clientRecords = dbConsumer.poll(Long.MAX_VALUE);
             System.out.println("Number of records fetched from DB: " + clientRecords.count());
 
             for (int i = 0; i < 3; i++) {
                 for (ConsumerRecord<Long, String> record : clientRecords) {
+                    //Parse JSON string to JSON object
                     JSONObject json = new JSONObject(record.value());
+                    //Instantiate client from payload part of json object
+                    //Which contains all the relevant data
                     Client client = gson.fromJson(json.get("payload").toString(), Client.class);
-
+                    //If client is not already in the pool
                     if (!clientIds.contains(client.getClient_id())) {
+                        //Add client to the pool
                         clientIds.add(client.getClient_id());
-                        System.out.println(json.get("payload"));
-                        System.out.println(client.getClient_name());
+                        System.out.println("Added client: " + client.getClient_name());
                     }
                 }
+                System.out.println("Number of clients currently in the pool: " + clientIds.size());
 
-                System.out.println("Number of clients: " + clientIds.size());
                 //Produce random credit
                 cred = rand.nextFloat() * (1000f - 1f);
 
                 //Choose random client to attach to the credit
-                index = rand.nextInt(clientIds.size());
-//            System.out.println(index);
+                clientId = clientIds.get(rand.nextInt(clientIds.size()));
 
-                //Get client Id
-                clientId = clientIds.get(index);
-
-                //Produce to topic
+                //Produce to credits topic
                 producer.send(new ProducerRecord<Long, Float>(cTopic, (long) clientId, cred));
-
                 System.out.println("Client " + clientId + " made a credit of " + cred + "euros.");
 
                 //Produce random pay
                 pay = rand.nextFloat() * (1000f - 1f);
 
-                //Choose random client to attach to the credit
-                index = rand.nextInt(clientIds.size());
-//            System.out.println(index);
+                //Choose random client to attach to the payment
+                clientId = clientIds.get(rand.nextInt(clientIds.size()));
 
-                //Choose random client to attach to the credit
-                clientId = clientIds.get(index);
-                //Produce to topic
+                //Produce to payments topic
                 producer.send(new ProducerRecord<Long, Float>(pTopic, (long) clientId, pay));
-
                 System.out.println("Client " + clientId + " made a payment of " + pay + "euros.");
 
                 //Sleep
-                Thread.sleep(5000);
+                Thread.sleep(sleepTime);
             }
         }
         //producer.close();
--- KafkaStreams/src/main/java/KafkaStreams.java
+++ KafkaStreams/src/main/java/KafkaStreams.java
@@ -3,8 +3,10 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
 
 import java.util.ArrayList;
@@ -15,118 +17,37 @@ import java.util.Properties;
 public class KafkaStreams {
     public static void main(String args[]) throws Exception {
 
-        //Setup Consumer Settings for KafkaStreams application
-        //Assign Credits and Payments as name topics
-        String cTopic = "Credits";
+        //Set stream properties
+        java.util.Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "exercises-application");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Long().getClass());
+
+        //Set topic names
+        String cTopic = "Clients";
         String pTopic = "Payments";
         String dbTopic = "DBInfoTopics";
-        // create instance for properties to access producer configs
-        Properties propsCPConsumer = new Properties();
-        //Assign localhost id
-        propsCPConsumer.put("bootstrap.servers", "localhost:9092");
-        //Set acknowledgements for producer requests.
-        propsCPConsumer.put("acks", "all");
-        //If the request fails, the producer can automatically retry,
-        propsCPConsumer.put("retries", 0);
-        //Specify buffer size in config
-        propsCPConsumer.put("batch.size", 16384);
-        //Reduce the no of requests less than 0
-        propsCPConsumer.put("linger.ms", 1);
-        //The buffer.memory controls the total amount of memory available to the producer for buffering.
-        propsCPConsumer.put("buffer.memory", 33554432);
-        propsCPConsumer.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaExampleConsumer");
-        propsCPConsumer.put("key.deserializer",
-                "org.apache.kafka.common.serialization.LongDeserializer");
-        propsCPConsumer.put("value.deserializer",
-                "org.apache.kafka.common.serialization.FloatDeserializer");
-        //Database consumer has different properties because it needs to have different key and value deserializers
-        // create instance for properties to access producer configs
-        Properties propsDBConsumer = new Properties();
-        //Assign localhost id
-        propsDBConsumer.put("bootstrap.servers", "localhost:9092");
-        //Set acknowledgements for producer requests.
-        propsDBConsumer.put("acks", "all");
-        //If the request fails, the producer can automatically retry,
-        propsDBConsumer.put("retries", 0);
-        //Specify buffer size in config
-        propsDBConsumer.put("batch.size", 16384);
-        //Reduce the no of requests less than 0
-        propsDBConsumer.put("linger.ms", 1);
-        //The buffer.memory controls the total amount of memory available to the producer for buffering.
-        propsDBConsumer.put("buffer.memory", 33554432);
-        propsDBConsumer.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaExampleConsumer");
-        propsDBConsumer.put("key.deserializer",
-                "org.apache.kafka.common.serialization.StringDeserializer");
-        propsDBConsumer.put("value.deserializer",
-                "org.apache.kafka.common.serialization.FloatDeserializer");
-        Consumer<Long, Float> cConsumer= new KafkaConsumer<>(propsCPConsumer);
-        Consumer<Long, Float> pConsumer = new KafkaConsumer<>(propsCPConsumer);
-        Consumer<String, Float> dbConsumer = new KafkaConsumer<>(propsDBConsumer);
-        //Set each consumer to their topic
-        cConsumer.subscribe(Collections.singletonList(cTopic));
-        cConsumer.subscribe(Collections.singletonList(pTopic));
-        dbConsumer.subscribe(Collections.singletonList(dbTopic));
-
-        //Setup properties to produce results to ResultsTopics
         String rTopic = "ResultsTopics";
-        //Assign Credits and Payments as name topics
-        // create instance for properties to access producer configs
-        Properties propsProducer = new Properties();
-        //Assign localhost id
-        propsProducer.put("bootstrap.servers", "localhost:9092");
-        //Set acknowledgements for producer requests.
-        propsProducer.put("acks", "all");
-        //If the request fails, the producer can automatically retry,
-        propsProducer.put("retries", 0);
-        //Specify buffer size in config
-        propsProducer.put("batch.size", 16384);
-        //Reduce the no of requests less than 0
-        propsProducer.put("linger.ms", 1);
-        //The buffer.memory controls the total amount of memory available to the producer for buffering.
-        propsProducer.put("buffer.memory", 33554432);
-        propsProducer.put("key.serializer", "org.apache.kafka.common.serialization.LongSerializer");
-        propsProducer.put("value.serializer", "org.apache.kafka.common.serialization.FloatSerializer");
-        //Create producer with previous properties
-        Producer<Long, Float> producer = new KafkaProducer<>(propsProducer);
+
         //Instantiate new stream builder
         StreamsBuilder builder = new StreamsBuilder();
 
-        //Get list of currencies from DBInfoTopics
-//        System.out.println("Getting clients from "+ dbTopic);
-//        ConsumerRecords<String, Float> clientRecords = dbConsumer.poll(Long.MAX_VALUE);
-//        System.out.println("Number of records fetched from DB: " + clientRecords.count());
-//        List<String> currencyList = new ArrayList<String>();
-//        for ( ConsumerRecord<String, Float> record:
-//                clientRecords) {
-//            currencyList.add(record.key());
-//        }
+        KStream<Long, Float> creditsStream = builder.stream(cTopic);
+        KStream<Long, Float> paymentsStream = builder.stream(pTopic);
+        KStream<Long, String> clientsStream = builder.stream(dbTopic);
 
         //Consume while there are resources to consume
         while (true) {
-            //Get records from Credits topic
-            ConsumerRecords<Long, Float> creditRecords = cConsumer.poll(Long.MAX_VALUE);
-            for (ConsumerRecord<Long, Float> record : creditRecords) {
-                System.out.println("Client " +record.key() + " made a credit of " + record.value() + "euros.");
+            //Read from credits topic and sum all credits for each user
+            KTable<Long, Float> creditLines = creditsStream.groupByKey().reduce((oldval, newval) -> oldval + newval, Materialized.as("tablename"));;
+            creditLines.toStream().to(rTopic);
+            System.out.println(creditLines);
 
-                //Outputs a stream to ResultsTopic with key:clientId and value:count of credits
-//                KStream<Long, Long> lines = builder.stream(cTopic);
-//                KTable<Long, Long> outlines = lines.
-//                        groupByKey().count();
-//                outlines.toStream().to(rTopic);
-            }
-
-            //Get records from Payments topic
-//            ConsumerRecords<Long, Float> paymentRecords = cConsumer.poll(Long.MAX_VALUE);
-//            for (ConsumerRecord<Long, Float> record : paymentRecords) {
-//                System.out.println("Client " +record.key() + " made a payment of " + record.value() + "euros.");
-//
-//                //Outputs a stream to ResultsTopic with key:clientId and value:count of credits
-////                KStream<Long, Long> lines = builder.stream(pTopic);
-////                KTable<Long, Long> outlines = lines.
-////                        groupByKey().count();
-////                outlines.toStream().to(rTopic);
-//
-//            }
+            //Read from payments topic and sum all payments for each user
+            KTable<Long, Float> paymentLines = paymentsStream.groupByKey().reduce((oldval, newval) -> oldval + newval, Materialized.as("tablename"));;
+            paymentLines.toStream().to(rTopic);
+            System.out.println(paymentLines);
         }
 
         //cConsumer.close();
