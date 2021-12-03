4cad25cbe53a2b4fdf6bc05f3494902c62e4a8d4
Rita00 <rita.lapao00@gmail.com>
2021-12-03 19:35:56 +0000

    streams printing

--- SolveApp/src/main/java/SolveApp.java
+++ SolveApp/src/main/java/SolveApp.java
@@ -1,14 +1,12 @@
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Materialized;
-import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -17,7 +15,6 @@ import java.util.Properties;
 
 public class SolveApp {
     public static void main(String args[]) throws Exception {
-
         //Set stream properties
         java.util.Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "SolveApp");
@@ -26,7 +23,7 @@ public class SolveApp {
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Float().getClass());
 
         //Set topic names
-        String cTopic = "Clients";
+        String cTopic = "Credits";
         String pTopic = "Payments";
         String dbTopic = "DBInfoTopics";
         String rTopic = "ResultsTopics";
@@ -34,27 +31,40 @@ public class SolveApp {
         //Instantiate new stream builder
         StreamsBuilder builder = new StreamsBuilder();
 
-        KStream<Long, Float> creditsStream = builder.stream(cTopic);
-        KStream<Long, Float> paymentsStream = builder.stream(pTopic);
-        KStream<Long, String> clientsStream = builder.stream(dbTopic);
+        KStream<Long, Float> creditsStream = builder.stream(cTopic, Consumed.with(Serdes.Long(), Serdes.Float()));
+        KStream<Long, Float> paymentsStream = builder.stream(pTopic, Consumed.with(Serdes.Long(), Serdes.Float()));
+        KStream<Long, String> clientsStream = builder.stream(dbTopic, Consumed.with(Serdes.Long(), Serdes.String()));
         System.out.println("Created KStreams...");
 
 
+        //Consume while there are resources to consume
+//        while (true) {
+
+        //Read from credits topic and sum all credits for each user
+//            KTable<Long, Float> creditLines = builder.table(cTopic);
+//            KTable<Long, Long> creditLines = creditsStream.groupByKey().count();
+//            creditLines.toStream().to("ResultsTopics", Produced.with(Serdes.Long(), Serdes.Long()));
+//            System.out.println("\n\n\n\nAqui\n\n\n\n");
+//            creditLines.toStream().print(Printed.toSysOut());
+//            System.out.println(creditLines);
+
+//        creditsStream.foreach((key, value) -> System.out.println(key + " =>---------------------- " + value));
+//            creditLines.mapValues((k, v) -> k + " => " + v).toStream().to(rTopic);
+
+        //Read from payments topic and sum all payments for each user
+//            KTable<Long, Float> paymentLines = paymentsStream.groupByKey().reduce((oldval, newval) -> oldval + newval, Materialized.as("tablename"));
+
+        creditsStream.peek((key, value) -> System.out.println("Client " + key + " has made " + value + " euros in credits."));
+
+        paymentsStream.peek((key, value) -> System.out.println("Client " + key + " has made " + value + " euros in payments."))/*.to(rTopic)*/;
+
         KafkaStreams streams = new KafkaStreams(builder.build(), props);
         streams.start();
-        System.out.println("Starting streams reading from topic " + cTopic);
-
-        //Consume while there are resources to consume
-        while (true) {
-
-            //Read from credits topic and sum all credits for each user
-            KTable<Long, Long> creditLines = creditsStream.groupByKey().count();
-            creditLines.mapValues((k, v) -> k + " => " + v).toStream().to(rTopic);
-
-            //Read from payments topic and sum all payments for each user
-            KTable<Long, Float> paymentLines = paymentsStream.groupByKey().reduce((oldval, newval) -> oldval + newval, Materialized.as("tablename"));;
-            paymentLines.toStream().peek((key, value) -> System.out.println("Client " + key +" has made " + value + " euros in payments.")).to(rTopic);
-        }
+        while(true);
+//        System.out.println("Starting streams reading from topic " + cTopic);
+//        Thread.sleep(30000);
+//        streams.close();
+//        }
 
         //cConsumer.close();
         //pConsumer.close();
